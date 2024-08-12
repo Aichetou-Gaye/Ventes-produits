f4b976e1b3cbfde371898b49817bdef55aaf0112
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-08-12 17:02:08 +0100

    added components

---
+++ src/components/FormulaireProd.vue
@@ -0,0 +1,48 @@
+<template>
+  <form class="row gy-2 gx-3 align-items-center" @submit="onSubmit">
+    <div class="col-sm-3">
+      <label for="ref">Reference</label>
+      <input type="text" class="form-control" id="ref">
+    </div>
+    <div class="col-auto">
+      <label for="desg">Designation</label>
+      <input type="text" class="form-control" id="desg">
+    </div>
+    <div class="col-auto">
+      <label for="qtite">Quantité</label>
+      <input type="number" class="form-control" id="qtite">
+    </div>
+    <div class="col-auto">
+      <label for="prix">Prix</label>
+      <input type="number" class="form-control" id="prix">
+    </div>
+
+    <div class="col-auto">
+      <button type="submit" class="btn btn-primary">Enregistrer</button>
+    </div>
+  </form>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const reference = ref('')
+const designation = ref('')
+const qtite = ref(1)
+const prix = ref(0)
+
+function onSubmit() {
+  try {
+    if (!reference.value || !designation.value || !qtite.value || !prix.value) {
+      throw new Error("Veuillez entrer des données valide")
+    }
+  } catch (e) {
+    console.log(e.message)
+  }
+  reference.value = ""
+  designation.value = ""
+  qtite.value = ""
+  prix.value = ""
+}
+
+</script>
---
+++ src/components/ListVente.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <table class="table table-bordered table-sm">
+    <thead>
+      <tr>
+        <td>#</td>
+        <td>Reference</td>
+        <td>Désignation</td>
+        <td>Quantité Vendue</td>
+        <td>Prix de vente</td>
+        <td>Montant</td>
+      </tr>
+    </thead>
+    <tbody v-for="(produit, index) in produits" :key="index">
+      <td>{{ index + 1 }}</td>
+      <td>{{ produit.reference }}</td>
+      <td>{{ produit.designation }}</td>
+      <td>{{ produit.qtite }}</td>
+      <td>{{ produit.prix }}</td>
+      <td>{{ produit.qtite * produit.prix }}</td>
+    </tbody>
+    <tfoot>
+      <tr>
+        <td colspan="3" class="text-center">Total</td>
+        <td>{{ total }}</td>
+      </tr>
+    </tfoot>
+  </table>
+
+</template>
+
+<script setup>
+
+// const props = defineProps({
+//   produits : {
+//     type : Array,
+
+//   }
+// })
+
+</script>
+
+
+<style scoped></style>
