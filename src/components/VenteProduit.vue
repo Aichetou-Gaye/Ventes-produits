3e44dc392b9fa094d6a8f2b330a5d98460e3a376
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-08-12 10:04:09 +0100

    second commit

--- src/components/VenteProduit.vue
+++ src/components/VenteProduit.vue
@@ -1,42 +1,84 @@
 <template>
   <div class="form">
 
-    <form @submit="onSubmit" @reset="onReset">
-      <h1>Enregistrement de Produits
-      </h1>
-      <label for="input-1">Reference:</label>
-      <input id="input-1" v-model="reference" type="text" placeholder="Entrez la référence du produit" required>
+    <form @submit.prevent="onSubmit">
+      <h1>Enregistrement de Produits</h1>
+      <div class="row">
+        <div class="label">
+          <label for="input-1">Reference:</label>
+          <input id="input-1" v-model="reference" type="text" placeholder="Entrez la référence du produit" required>
+        </div>
+        <div class="label">
+          <label for="input-2">Désignation:</label>
+          <input id="input-2" v-model="designation" type="text" placeholder="Entrez la désignation" required>
+        </div>
+      </div>
+      <div class="row">
+        <div class="label">
+          <label for="input-3">Quantité Vendue:</label>
+          <input id="input-3" v-model="qtite" type="number" placeholder="Entrez la quantité vendue du produit" required>
+        </div>
+        <div class="label">
+          <label for="input-4">Prix de vente:</label>
+          <input id="input-4" v-model="prix" type="number" placeholder="Entrez le prix du produit" required>
+        </div>
 
-      <label for="input-2">Désignation:</label>
-      <input id="input-2" v-model="designation" placeholder="Entrez la désignation" required>
-
-      <label for="input-3">Quantité Vendue:</label>
-      <input id="input-3" v-model="qtite" placeholder="Entrez la quantité vendue du produit" required>
-
-      <label for="input-4">Prix de vente:</label>
-      <input id="input-4" v-model="prix" placeholder="Entrez le prix du produit" required>
+      </div>
 
       <button type="submit">Enregistrer</button>
-      <button type="reset">Supprimer</button>
     </form>
+    <table>
+      <thead>
+        <tr>
+          <td>Reference</td>
+          <td>Désignation</td>
+          <td>Quantité Vendue</td>
+          <td>Prix de vente</td>
+        </tr>
+      </thead>
+      <tbody v-for="produit in produit" :key="produit.id">
+        <td>{{ produit.r }}</td>
+        <td>{{ produit.d }}</td>
+        <td>{{ produit.q }}</td>
+        <td>{{ produit.p }}</td>
+      </tbody>
+    </table>
+
   </div>
 
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
+
+const produit = reactive([{
+  id: Date.now(),
+  r: "",
+  d: "",
+  q: "",
+  p: ""
+}
+])
 const reference = ref('')
 const designation = ref('')
 const qtite = ref('')
 const prix = ref('')
 
 function onSubmit() {
-
+  try {
+    if (!reference.value || !designation.value || !qtite.value || !prix.value) {
+      throw new Error("Veuillez entrer des données valide")
+    }
+  } catch (e) {
+    console.log(e.message)
+  }
+  produit.push({ r: reference.value, d: designation.value, q: qtite.value, p: prix.value })
+  reference.value = ""
+  designation.value = ""
+  qtite.value = ""
+  prix.value = ""
 }
 
-function onReset() {
-
-}
 </script>
 
 
@@ -50,18 +92,21 @@ form {
   color: #FFFFFF;
   padding: 30px 60px;
   border-radius: 10px;
-  opacity: 0.95;
-  width: 45%;
+  width: 55%;
   margin: auto;
   font-family: "Montserrat Alternates", sans-serif;
 }
-
 label {
   font-size: 1em;
+}
+.row{
+  display: flex;
+  justify-content: space-between;
 }
 h1 {
   font-size: 1.2em;
   text-align: center;
+  margin-bottom: 20px
 }
 input {
   border-radius: 5px;
@@ -70,6 +115,8 @@ input {
   border: none;
   font-family: "Montserrat Alternates", sans-serif;
   color: #000000;
+  width: 60%;
+  font-size: 0.9em;
 }
 
 input::placeholder {
@@ -80,6 +127,12 @@ input:focus {
   background-color: #000000;
   color: #ffffff;
   outline: none;
+}
+.label {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 }
 
 button {
@@ -98,5 +151,21 @@ button {
 button:hover {
   color: #228d5b;
   background-color: #ffffff;
+}
+.form {
+  display: flex;
+  flex-direction: column;
+}
+thead {
+  color: #ffffff;
+  background-color: #228d5b;
+  border: 1px solid #fff;
+}
+
+tbody {
+  color: #000;
+  background-color: #ffffff ;
+  border: 1px solid #000;
+  text-align: center;
 }
 </style>
